c33491bba7a5955a87d8da5795d68db893e1fc93
Matt Schlenker <mas2363@columbia.edu>
2014-09-05 20:31:21 -0400

    Adds fundamental move dirs for pieces.

--- Piece.rb
+++ Piece.rb
@@ -1,6 +1,13 @@
+KNIGHT_DIRS = [[-1,-2],[-1,2],[1,-2],[1,2],[-2,-1],[-2,1],[2,-1],[2,1]]
+BISHOP_DIRS = [[-1,-1], [-1,1], [1,-1], [1,1]]
+ROOK_DIRS = [[-1,0], [1,0], [0,-1], [0,1]]
+
+#Queen and King dirs are the combination of Bishop and Rook dirs
+#Pawn dirs are at most three, but more variable, easier to handle directly in class
+
 class Piece
   def initialize(color)
-    self.color = color
+    @color = color
   end
 end
 
@@ -25,5 +32,5 @@ end
 class Knight < SteppingPiece
 end
 
-class Pawn
+class Pawn < Piece
 end
