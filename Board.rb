38f014ef17481af52e89a6631f45436922e55ec7
Matt Schlenker <mas2363@columbia.edu>
2014-09-07 18:00:46 -0400

    King and Queen do not need to flip positions based on side...whoops.

--- Board.rb
+++ Board.rb
@@ -5,8 +5,8 @@ class Board
   
   def self.create_grid
     matrix = Array.new(4) { Array.new(8) }
-    matrix.unshift(Board.place_pawns("W")).push(Board.place_pawns("B"))
-    matrix.unshift(Board.place_royalty("W")).push(Board.place_royalty("B"))
+    matrix.unshift(Board.place_pawns("B")).push(Board.place_pawns("W"))
+    matrix.unshift(Board.place_royalty("B")).push(Board.place_royalty("W"))
     
     matrix
   end
@@ -21,16 +21,16 @@ class Board
   
   def self.place_royalty(color)
     y = (color == "B") ? 0 : 7
+    
     royalty = [
-                Rook.new([0,y], color),
-                Knight.new([1,y], color),
-                Bishop.new([2,y], color),
-                Queen.new([3,y], color),
-                King.new([4,y], color),
-                Bishop.new([5,y], color),
-                Knight.new([6,y], color),
-                Rook.new([7,y], color)
-              ]
+      Rook.new([0, y], color), 
+      Knight.new([1, y], color),
+      Bishop.new([2, y], color),
+      Queen.new([3, y], color),
+      King.new([4, y], color),
+      Bishop.new([5, y], color),
+      Knight.new([6, y], color),
+      Rook.new([7, y], color)]
   end
   
   def initialize
